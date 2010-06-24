70d778dbc17639612a5bb514f822729c9c9021ca
mausch <mauricioscheffer@gmail.com>
2010-06-24 19:20:31 -0300

    checking for initialized perwebrequest module

--- Castle.Windsor.Lifestyles/HybridPerWebRequestLifestyleManager.cs
+++ Castle.Windsor.Lifestyles/HybridPerWebRequestLifestyleManager.cs
@@ -14,6 +14,7 @@
 // limitations under the License.
 #endregion
 
+using System.Reflection;
 using System.Web;
 
 namespace Castle.MicroKernel.Lifestyle {
@@ -23,8 +24,18 @@ namespace Castle.MicroKernel.Lifestyle {
     /// <typeparam name = "T">Secondary lifestyle</typeparam>
     public class HybridPerWebRequestLifestyleManager<T> : HybridLifestyleManager<PerWebRequestLifestyleManager, T>
         where T : ILifestyleManager, new() {
+
+        // TODO make this public in Windsor
+        private static readonly PropertyInfo PerWebRequestLifestyleModuleInitialized = typeof (PerWebRequestLifestyleModule).GetProperty("Initialized", BindingFlags.Static | BindingFlags.NonPublic);
+
+        private static bool IsPerWebRequestLifestyleModuleInitialized {
+            get {
+                return (bool) PerWebRequestLifestyleModuleInitialized.GetValue(null, null);
+            }
+        }
+
         public override object Resolve(CreationContext context) {
-            if (HttpContext.Current != null)
+            if (HttpContext.Current != null && IsPerWebRequestLifestyleModuleInitialized)
                 return lifestyle1.Resolve(context);
             return lifestyle2.Resolve(context);
         }
