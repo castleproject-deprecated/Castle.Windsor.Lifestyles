675baab5ceb95a415f38c75c4779157578155927
mausch <mauricioscheffer@gmail.com>
2010-06-24 17:39:24 -0300

    license headers

--- Castle.MicroKernel.Lifestyle.Extras/PerHttpApplicationLifestyleManager.cs
+++ Castle.MicroKernel.Lifestyle.Extras/PerHttpApplicationLifestyleManager.cs
@@ -1,6 +1,5 @@
 ﻿#region license
-// Copyright 2004-2009 Castle Project - http://www.castleproject.org/
-// Copyright (c) 2009 Mauricio Scheffer
+// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
