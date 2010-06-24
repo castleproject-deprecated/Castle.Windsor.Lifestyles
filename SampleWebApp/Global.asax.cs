448da68fe826cf8724c94c01af2f6cc0de4179fe
mausch <mauricioscheffer@gmail.com>
2010-06-24 19:22:04 -0300

    sample web app

---
+++ SampleWebApp/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web;
+using Castle.Facilities.FactorySupport;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+
+namespace SampleWebApp {
+    public class Global : HttpApplication {
+        protected void Application_Start(object sender, EventArgs e) {
+            var kernel = new DefaultKernel();
+            kernel.AddFacility<FactorySupportFacility>();
+            kernel.Register(Component.For<HttpContextBase>()
+                                .LifeStyle.Transient
+                                .UsingFactoryMethod(() => new HttpContextWrapper(HttpContext.Current)));
+            kernel.Register(Component.For<SomeService>()
+                .LifeStyle.HybridPerWebRequestTransient());
+            kernel.Resolve<SomeService>();
+        }
+
+        public class SomeService {
+            public SomeService(HttpContextBase req) {}
+        }
+
+        protected void Session_Start(object sender, EventArgs e) {}
+
+        protected void Application_BeginRequest(object sender, EventArgs e) {}
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e) {}
+
+        protected void Application_Error(object sender, EventArgs e) {}
+
+        protected void Session_End(object sender, EventArgs e) {}
+
+        protected void Application_End(object sender, EventArgs e) {}
+    }
+}
